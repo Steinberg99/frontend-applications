0beaa3a740771234977583c918e10e3e4b62f80c
Steinberg99 <stein.bergervoet@gmail.com>
2021-11-22 14:05:58 +0100

    Add Map component to draw a map with GeoJSON url

--- src/App.js
+++ src/App.js
@@ -5,8 +5,26 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Map></Map>
-      <Chart></Chart>
+      <Map
+        id="provices"
+        geoJSONUrl="https://cartomap.github.io/nl/wgs84/provincie_2021.geojson"
+      ></Map>
+      <Map
+        id="townships"
+        geoJSONUrl="https://cartomap.github.io/nl/wgs84/gemeente_2021.geojson"
+      ></Map>
+      <Map
+        id="tourism-areas"
+        geoJSONUrl="https://cartomap.github.io/nl/wgs84/toeristengebied_2021.geojson"
+      ></Map>
+      <Map
+        id="ggd-regions"
+        geoJSONUrl="https://cartomap.github.io/nl/wgs84/ggdregio_2021.geojson"
+      ></Map>
+      <Map
+        id="france"
+        geoJSONUrl="https://france-geojson.gregoiredavid.fr/repo/departements.geojson"
+      ></Map>
     </div>
   );
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as d3 from "d3";
 import "./Map.css";
 
-const Map = () => {
-  const naam = "Stein";
+function Map(props) {
+  const [geoJSONData, setGeoJSONData] = useState({});
+
+  useEffect(() => {
+    fetchGeoJSONData(props.geoJSONUrl);
+  }, []);
+
+  const fetchGeoJSONData = async () => {
+    try {
+      const response = await fetch(props.geoJSONUrl);
+      const data = await response.json();
+      setGeoJSONData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  drawMap(props.id, geoJSONData);
+
   return (
-    <div className="Map">
-      <h1>Map component gemaakt door {naam}!</h1>
+    <div>
+      <h1>{props.id}</h1>
+      <svg id={props.id} className="Map"></svg>
     </div>
   );
-};
+}
+
+function drawMap(id, geoJSONData) {
+  const width = 500;
+  const height = 500;
+
+  const projection = d3.geoMercator().fitSize([width, height], geoJSONData);
+  const path = d3.geoPath(projection);
+
+  d3.select(`#${id}`)
+    .append("g")
+    .selectAll("path")
+    .data(geoJSONData.features)
+    .enter()
+    .append("path")
+    .attr("d", path);
+}
 
 export default Map;
