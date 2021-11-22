7dbae9c269a5cf24041edb7b1db39c31877af6f0
Steinberg99 <stein.bergervoet@gmail.com>
2021-11-22 12:21:13 +0100

    Create React project

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Chart.css";
+
+const Chart = () => {
+  return (
+    <div className="Chart">
+      <h1>Chart component</h1>
+    </div>
+  );
+};
+
+export default Chart;
---
+++ src/components/Map/Map.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Map.css";
+
+const Map = () => {
+  const naam = "Stein";
+  return (
+    <div className="Map">
+      <h1>Map component gemaakt door {naam}!</h1>
+    </div>
+  );
+};
+
+export default Map;
